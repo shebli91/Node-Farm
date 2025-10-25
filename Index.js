7eae7c98960dfa3807d9e88cdc4945170223e992
Shebli91 <mhamadshebli@gmail.com>
2025-10-26 01:06:25 +0300

    importing slugify and create a function to slugify the product names

--- Index.js
+++ Index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const { json } = require("stream/consumers");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./Modules/replaceTemplate");
 
 /////////////////////////////////////// FILES //////////////////////////////////////////
@@ -48,6 +50,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
