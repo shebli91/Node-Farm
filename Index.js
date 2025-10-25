594b60c44a4b1d571fc64c484890bb5e2a36b3cd
Shebli91 <mhamadshebli@gmail.com>
2025-10-26 00:17:51 +0300

    Creating replaceTemplate as a module and export it then import it to the index.js

--- Index.js
+++ Index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const { json } = require("stream/consumers");
 const url = require("url");
 
+const replaceTemplate = require("./Modules/replaceTemplate");
+
 /////////////////////////////////////// FILES //////////////////////////////////////////
 // Blocking , Synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -30,21 +32,6 @@ const url = require("url");
 // console.log("Will read file!");
 
 /////////////////////////////////////// SERVER  //////////////////////////////////////////
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
